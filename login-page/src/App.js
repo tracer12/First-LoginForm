33dd2ca6fa4ad262e7ff90bc51429edc1b55d3c2
tracer12 <vendetta12@naver.com>
2024-01-26 20:15:53 +0900

    test

---
+++ login-page/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import LoginForm from './components/LoginForm/LoginForm';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <LoginForm />
+    </div >
+
+  );
+}
+
+export default App;
